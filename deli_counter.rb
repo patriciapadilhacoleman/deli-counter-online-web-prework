dffdcb813d609a28e74e2280f7cfea87e1643aed
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-11 20:38:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,30 @@
 # Write your code here.
-def line
-  "The line is currently empty."
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.count = 0 
+    puts "The line is currently empty."
+    
+  else
+    
+    katz_deli.each do |name|
+      
+      
+      
+    end
+    
+  end 
+
 end
 
 def take_a_number(katz_deli, name)
   
-  puts "Welcome, #{name}"
+  puts "Welcome, #{name} You are number #{number} in line."
   
 end
 
 def now_serving(katz_deli, name)
   
-  if 
+
   
 end
